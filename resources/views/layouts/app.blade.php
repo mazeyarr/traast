1de266e3a1ce97032947787725326b8338b459d8
mazeyarr <mazeyarr@gmail.com>
2018-07-09 16:47:53 +0200

    init

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+
+<script src="{{ asset('js/jquery-3.3.1.min.js') }}"></script>
+<script src="{{ asset('bootstrap/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
